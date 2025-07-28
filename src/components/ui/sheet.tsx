52d8077a2ce0d54f758d4aacbe87ec1efd7dd93c
Stephen Bozzone <sbozzone@gmail.com>
2025-07-28 15:21:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -126,6 +126,32 @@ const SheetDescription = React.forwardRef<
 ))
 SheetDescription.displayName = SheetPrimitive.Description.displayName
 
+const VisuallyHidden = React.forwardRef<
+  HTMLSpanElement,
+  React.HTMLAttributes<HTMLSpanElement>
+>((props, ref) => {
+  return (
+    <span
+      ref={ref}
+      style={{
+        position: 'absolute',
+        border: 0,
+        width: 1,
+        height: 1,
+        padding: 0,
+        margin: -1,
+        overflow: 'hidden',
+        clip: 'rect(0, 0, 0, 0)',
+        whiteSpace: 'nowrap',
+        wordWrap: 'normal',
+      }}
+      {...props}
+    />
+  );
+});
+VisuallyHidden.displayName = 'VisuallyHidden';
+
+
 export {
   Sheet,
   SheetPortal,
@@ -137,4 +163,5 @@ export {
   SheetFooter,
   SheetTitle,
   SheetDescription,
+  VisuallyHidden,
 }
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetTitle, SheetDescription, VisuallyHidden } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -206,6 +206,13 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <VisuallyHidden>
+              <SheetTitle>Sidebar</SheetTitle>
+              <SheetDescription>
+                Contains the main navigation and application tools.
+              </SheetDescription>
+            </VisuallyHidden>
+
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
